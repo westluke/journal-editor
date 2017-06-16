56e6965165c0b79418ed6174c0e02cb55d695ead
Luke West <l.s.west88@gmail.com>
2017-06-16 16:00:39 -0400

    HA didn't actually add main.cpp for that last commit.

--- main.cpp
+++ main.cpp
@@ -2,54 +2,38 @@
 #include <csignal>
 #include <iostream>
 #include <atomic>
+#include <chrono>
+#include <thread>
+#include <string>
+#include <sstream>
+
+void print_next_line(std::string);
 
 
-void temp_handler(int sig) {
-	raise(SIGUSR1);
-}
+/*
+ * AAAGH ncurses handles pushing the KEY_RESIZE character itself! FAK ME!
+ *
+ *
+ *
+// Prevents the thread from putting more than one character at a time into the buffer with ungetch().
+std::atomic_bool resize_flag;
 
-// Yes, this worked.
-// This seems to work fine so far, and it seems like most of the time functions like getch() are implemented so as to be thread-safe.
-// So maybe I don't need any kind of lock.
-// How would I lock it? There are a few things I have to guarantee. EVERY screen change must be detected and acted upon.
-// At the same time, I definitely can't ungetch() more than one character at a time/
-// Remember that almost all the time, the window resize will happen during a call to getch().
-// DURING. That means ungetch() HAS to occur DURING the getch(), so locking is sorta pointless.
-// What ISN'T pointless is ensuring that no more than one character is pushed back at a time.
-// What would have to happen to push back more than one character? During a getch(), a signal is handled.
-// BEFORE THE GETCH() COMPLETES, ANOTHER SIGNAL IS HANDLED. That would be pretty crazy. But still, is there any way to prevent it?
-// Yes, I think so.
-//
-//
-// The handler sets a global atomic to 1.
-// After completing, getch() sets it to zero.
-// After that, the program adjusts the window to the correct size. As long as getch() receives the right character, it doesn't matter how many signals were handled.
-//
-// Only if its zero can the handler ungetch().
-void resize_handler(int sig) {
-	std::cout << "WORKED" << std::endl;
-}
+// Prevents the case where the main thread sets the resize_flag to true between the call to ungetch() and the setting of resize_flag to false in the resize thread.
+std::mutex resize_mtx;
 
-
-// A lot of state required.
-// size_changed needs a representation of window size (no atomicity necessary)
-// Makes sense to use a static object inside the size_changed function.
-// need to store mutex lock, must also be visible to both threads, so global with internal linkage.
-// need to store flag atomically, must be visible to both threads, so global but internal linkage.
-
+bool size_changed(); 
+void thread_task(void);
 
 void thread_task(void) {
 	while (true){
-		if (flag){
-			if (size_changed()){
-				lock(mutex);
-				ungetch(KEY_RELEASE);
-				flag = FALSE;
-				unlock(mutex);
-			}
+		if (resize_flag && size_changed()){
+			resize_mtx.lock();
+			ungetch(KEY_RESIZE);
+			resize_flag = false;
+			resize_mtx.unlock();
 		}
 
-		delay();
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
 	}
 }
 
@@ -62,41 +46,65 @@ bool size_changed() {
 	if ((prev_rows != rows) || (prev_cols != cols)){ 
 		prev_rows = rows;
 		prev_cols = cols;
+
 		return true;
 	}
 
 	return false;
 }
+*/
+
+void print_next_line(std::string line) {
+	int y, x;
+	getyx(stdscr, y, x);
+	mvprintw(y + 1, 1, line.c_str());
+	refresh();
+}
 
 int main() {
+	initscr();	// Start curses mode
+	cbreak();	// Disable line-by-line buffering
+	noecho();	// Prevent entered characters from immediately appearing on screen
+
+	// Just one of keypad() or mousemask() is necessary to prevent scrollback.
+
+	// Makes events that have special key prefixes (like some mouse events) into single values like KEY_LEFT,
+	// instead of something like the combination Fn-5
+	keypad(stdscr, true);
+
+	// Without mousemask, no mouse events are reported.
+	// Scrolling gets converted to character codes instead, like KEY_UP or KEY_DOWN.
+	// mousemask(ALL_MOUSE_EVENTS, NULL);
+
 	int y, x;
-	
-	// Use of sa_sigaction and sa_handler overlap, that's why. So definitely don't use braced-init.
-	struct sigaction temp_handler_sigaction;
-	temp_handler_sigaction.sa_handler = temp_handler;
-	sigemptyset(&temp_handler_sigaction.sa_mask);
-	temp_handler_sigaction.sa_flags = SA_RESTART;
-
-	struct sigaction resize_handler_sigaction;
-	resize_handler_sigaction.sa_handler = resize_handler;
-	sigemptyset(&resize_handler_sigaction.sa_mask);
-	resize_handler_sigaction.sa_flags = SA_RESTART;
-
-	sigaction(SIGWINCH, &temp_handler_sigaction, nullptr);
-	sigaction(SIGUSR1, &resize_handler_sigaction, nullptr);
-
-	initscr();			/* Start curses mode 		  */
-	cbreak();
-	noecho();
 	getmaxyx(stdscr, y, x);
-	move(0, 1);
+	move(0, 0);
 	vline('~', y);
-	refresh();			/* Print it on to the real screen */
+	move(0, 1);
+	refresh();	//Print it on to the real screen
 
-	while (!('a' == getch()))
-		;
-	// If you don't have this then things get crazy.
-	endwin();
+	int int_char;
 
-	return 0;
+	int cnt = 0;
+
+	while (true){
+		int_char = getch();
+
+		if (int_char == 'a'){
+			endwin();	// if you don't have this the window gets very messy
+			return 0;
+		}
+
+		if (int_char == 'g'){
+			ungetch(KEY_RESIZE);
+		}
+		
+		if (int_char == KEY_RESIZE){
+			int y, x;
+			getyx(stdscr, y, x);
+			mvprintw(y + 1, 1, "Captured KEY_RESIZE, count = %d", cnt++);
+			refresh();
+		}
+		
+	}
 }
